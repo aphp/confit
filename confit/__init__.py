7fc380358834fe9b8e09f0dfafd309f999ea58b5
Perceval Wajsbürt <perceval.wajsburt@aphp.fr>
2023-09-14 18:16:42 +0200

    feat: add deprecation parameter

--- confit/__init__.py
+++ confit/__init__.py
@@ -6,6 +6,7 @@ from .registry import (
     get_default_registry,  # noqa F401
     set_default_registry,  # noqa F401
     RegistryCollection,  # noqa F401
+    VisibleDeprecationWarning,  # noqa F401
 )
 
 __version__ = "0.4.3"
